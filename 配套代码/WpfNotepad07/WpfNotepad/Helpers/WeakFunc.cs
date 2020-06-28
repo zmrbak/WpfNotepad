beaaefc7b68dd6464ddadc98403db9f355ac74b8
zmrbak <20995658@qq.com>
2020-06-28 09:58:00 +0800

    add code

---
+++ 配套代码/WpfNotepad07/WpfNotepad/Helpers/WeakFunc.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNotepad.Helpers
+{
+    public class WeakFunc<TResult>
+    {
+        private Func<TResult> _staticFunc;
+        protected MethodInfo Method
+        {
+            get;
+            set;
+        }
+
+        public bool IsStatic
+        {
+            get
+            {
+                return _staticFunc != null;
+            }
+        }
+
+        public virtual string MethodName
+        {
+            get
+            {
+                if (_staticFunc != null)
+                {
+                    return _staticFunc.Method.Name;
+                }
+                return Method.Name;
+            }
+        }
+
+        protected WeakReference FuncReference
+        {
+            get;
+            set;
+        }
+
+        protected object LiveReference
+        {
+            get;
+            set;
+        }
+
+        protected WeakReference Reference
+        {
+            get;
+            set;
+        }
+
+        protected WeakFunc()
+        {
+        }
+
+        public WeakFunc(Func<TResult> func, bool keepTargetAlive = false)
+            : this(func == null ? null : func.Target, func, keepTargetAlive)
+        {
+        }
+
+        public WeakFunc(object target, Func<TResult> func, bool keepTargetAlive = false)
+        {
+            if (func.Method.IsStatic)
+            {
+                _staticFunc = func;
+
+                if (target != null)
+                {
+                    // Keep a reference to the target to control the
+                    // WeakAction's lifetime.
+                    Reference = new WeakReference(target);
+                }
+
+                return;
+            }
+
+            Method = func.Method;
+            FuncReference = new WeakReference(func.Target);
+
+            LiveReference = keepTargetAlive ? func.Target : null;
+            Reference = new WeakReference(target);
+        }
+
+        public virtual bool IsAlive
+        {
+            get
+            {
+                if (_staticFunc == null
+                    && Reference == null
+                    && LiveReference == null)
+                {
+                    return false;
+                }
+
+                if (_staticFunc != null)
+                {
+                    if (Reference != null)
+                    {
+                        return Reference.IsAlive;
+                    }
+
+                    return true;
+                }
+
+                if (LiveReference != null)
+                {
+                    return true;
+                }
+
+                if (Reference != null)
+                {
+                    return Reference.IsAlive;
+                }
+
+                return false;
+            }
+        }
+
+        public object Target
+        {
+            get
+            {
+                if (Reference == null)
+                {
+                    return null;
+                }
+
+                return Reference.Target;
+            }
+        }
+
+        protected object FuncTarget
+        {
+            get
+            {
+                if (LiveReference != null)
+                {
+                    return LiveReference;
+                }
+
+                if (FuncReference == null)
+                {
+                    return null;
+                }
+
+                return FuncReference.Target;
+            }
+        }
+
+        public TResult Execute()
+        {
+            if (_staticFunc != null)
+            {
+                return _staticFunc();
+            }
+
+            var funcTarget = FuncTarget;
+
+            if (IsAlive)
+            {
+                if (Method != null
+                    && (LiveReference != null
+                        || FuncReference != null)
+                    && funcTarget != null)
+                {
+                    return (TResult)Method.Invoke(funcTarget, null);
+                }
+            }
+            return default(TResult);
+        }
+
+        public void MarkForDeletion()
+        {
+            Reference = null;
+            FuncReference = null;
+            LiveReference = null;
+            Method = null;
+            _staticFunc = null;
+        }
+    }
+}
---
+++ 配套代码/WpfNotepad07/WpfNotepad/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Resources;
+using WpfNotepad.Helpers;
+
+namespace WpfNotepad.ViewModels
+{
+    public class MainViewModel
+    {
+        public HelpViewModel HelpViewModel { get; }
+        public MainViewModel()
+        {
+            HelpViewModel = new HelpViewModel();
+        }      
+    }
+}
