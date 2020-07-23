f84e362d04c0ed3653173b4e9c49dc515e001eff
zmrbak <20995658@qq.com>
2020-07-23 09:29:58 +0800

    add code

---
+++ 配套代码/WpfNotepad37/WpfNotepad/Interactivity/TriggerCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace WpfNotepad.Interactivity
+{
+    public sealed class TriggerCollection : AttachableCollection<TriggerBase>
+    {
+        internal TriggerCollection()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            foreach (TriggerBase triggerBase in this)
+            {
+                triggerBase.Attach(base.AssociatedObject);
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            foreach (TriggerBase triggerBase in this)
+            {
+                triggerBase.Detach();
+            }
+        }
+
+        internal override void ItemAdded(TriggerBase item)
+        {
+            if (base.AssociatedObject != null)
+            {
+                item.Attach(base.AssociatedObject);
+            }
+        }
+
+        internal override void ItemRemoved(TriggerBase item)
+        {
+            if (((IAttachedObject)item).AssociatedObject != null)
+            {
+                item.Detach();
+            }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new TriggerCollection();
+        }
+    }
+}
---
+++ 配套代码/WpfNotepad37/WpfNotepad/ViewModels/ReplaceWindowModel.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfNotepad.Helpers;
+using WpfNotepad.Models;
+using WpfNotepad.Utility;
+
+namespace WpfNotepad.ViewModels
+{
+    public class ReplaceWindowModel
+    {
+        public ReplaceWindowModel()
+        {
+            DocumentModel = (Application.Current.Resources["Locator"] as ViewModelLocator).DocumentModel;
+
+            FindTextCommand = new RelayCommand<TextBox>(OnFindTextCommand);
+            CancelCommand = new RelayCommand<Window>(OnCancelCommand);
+            ViewLoadedCommand = new RelayCommand<TextBox>(OnViewLoadedCommand);
+            ReplaceCommand = new RelayCommand(OnReplaceCommand);
+            ReplaceAllCommand = new RelayCommand(OnReplaceAllCommand);
+        }
+
+        private void OnReplaceAllCommand()
+        {
+            DocumentModel.TextBox.Text = Strings.Replace(
+                DocumentModel.TextBox.Text,
+                DocumentModel.FindText,
+                DocumentModel.ReplacedText,
+                1,
+                -1,
+                DocumentModel.IsFindCaseSensitive ? CompareMethod.Binary:CompareMethod.Text
+                );
+            DocumentModel.TextBox.SelectionLength = 0;
+            DocumentModel.TextBox.SelectedText = "";
+
+            DocumentModel.TextBox.ScrollToHome();
+        }
+
+        private void OnReplaceCommand()
+        {
+            if(DocumentModel.FindText.Equals(
+                DocumentModel.TextBox.SelectedText,
+                DocumentModel.IsFindCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase
+                ))
+            {
+                DocumentModel.TextBox.SelectedText = DocumentModel.ReplacedText;
+            }
+
+            Tools.OnFindTextCommand(DocumentModel,Direction.Down);
+        }
+
+        private void OnViewLoadedCommand(TextBox textBox)
+        {
+            textBox.SelectAll();
+            textBox.Focus();
+        }
+
+        private void OnCancelCommand(Window window)
+        {
+            DocumentModel.LoadSettings();
+            window.Close();
+        }
+
+        private void OnFindTextCommand(TextBox textBox)
+        {
+            Tools.OnFindTextCommand(DocumentModel);
+
+            textBox.Focus();
+
+            DocumentModel.SaveSettings();
+        }
+
+
+        public DocumentModel DocumentModel { get; }
+
+        public RelayCommand<TextBox> FindTextCommand { get; }
+        public RelayCommand<Window> CancelCommand { get; }
+        public RelayCommand<TextBox> ViewLoadedCommand { get; }
+        public RelayCommand ReplaceCommand { get; }
+        public RelayCommand ReplaceAllCommand { get; }
+
+    }
+}
